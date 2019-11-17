c1cb88adfe80071ba00091b1c8a492f375b1e9f8
MinexAutomation <minex.automation@gmail.com>
2019-11-16 16:24:53 -0800

    Moved static code to get file-extensions for file-formats to the plain ImageFileFormatOperator.

--- source/R5T.Sitonia.Default/Code/Services/Classes/PathBasedImageFileFormatOperator.cs
+++ source/R5T.Sitonia.Default/Code/Services/Classes/PathBasedImageFileFormatOperator.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 
 using R5T.Lombardy;
-using R5T.Magyar;
 using R5T.Philippi;
 using R5T.Sparta;
 
@@ -10,48 +9,6 @@ namespace R5T.Sitonia.Default
 {
     public class PathBasedImageFileFormatOperator : IPathBasedImageFileFormatOperator
     {
-        #region Static
-
-        public static FileFormat GetFileFormatAllowUnknown(FilePath imageFilePath, IStringlyTypedPathOperator stringlyTypedPathOperator)
-        {
-            var fileExtension = stringlyTypedPathOperator.GetFileExtension(imageFilePath.Value);
-
-            var upperFileExtension = fileExtension.ToUpperInvariant();
-            switch (upperFileExtension)
-            {
-                case "BMP":
-                    return FileFormat.BMP;
-
-                case "JPG":
-                case "JPEG":
-                    return FileFormat.JPG;
-
-                case "PNG":
-                    return FileFormat.PNG;
-
-                default:
-                    return FileFormat.Unknown;
-            }
-        }
-
-        public static FileFormat GetFileFormatNoUnknown(FilePath imageFilePath, IStringlyTypedPathOperator stringlyTypedPathOperator)
-        {
-            var fileFormat = PathBasedImageFileFormatOperator.GetFileFormatAllowUnknown(imageFilePath, stringlyTypedPathOperator);
-
-            if (fileFormat == FileFormat.Unknown)
-            {
-                var fileExtension = stringlyTypedPathOperator.GetFileExtension(imageFilePath.Value);
-
-                var message = EnumHelper.UnrecognizedEnumerationValueMessage<FileFormat>(fileExtension);
-                throw new Exception(message);
-            }
-
-            return fileFormat;
-        }
-
-        #endregion
-
-
         private IStringlyTypedPathOperator StringlyTypedPathOperator { get; }
 
 
@@ -61,11 +18,11 @@ namespace R5T.Sitonia.Default
         }
 
         /// <summary>
-        /// Uses the <see cref="PathBasedImageFileFormatOperator.GetFileFormatAllowUnknown(FilePath, IStringlyTypedPathOperator)"/> implementation as the default.
+        /// Uses the <see cref="ImageFileFormatOperator.GetFileFormatAllowUnknown(FilePath, IStringlyTypedPathOperator)"/> implementation as the default.
         /// </summary>
         public FileFormat GetFileFormat(FilePath imageFilePath)
         {
-            var fileFormat = PathBasedImageFileFormatOperator.GetFileFormatAllowUnknown(imageFilePath, this.StringlyTypedPathOperator);
+            var fileFormat = ImageFileFormatOperator.GetFileFormatAllowUnknown(imageFilePath, this.StringlyTypedPathOperator);
             return fileFormat;
         }
     }
